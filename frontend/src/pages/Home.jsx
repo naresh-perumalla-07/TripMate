ad61620ec0b2e00d6441bda93c59e0643b430f6c
naresh-perumalla-07 <naresh.xplores777@gmail.com>
2025-10-07 19:36:24 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -12,13 +12,13 @@ function Home() {
   
   return (
     <div>
-     <Nav/>
+     {/* <Nav/>
      <div className='w-[100vw]  flex items-center justify-center gap-[25px] flex-wrap mt-[250px] md:mt-[180px]'>
      {newListData.map((list)=>(
       <Card title={list.title} landMark={list.landMark} city={list.city} image1={list.image1} image2={list.image2} image3={list.image3} rent={list.rent} id={list._id} ratings={list.ratings} isBooked={list.isBooked} host={list.host}/>
      ))}
      </div>
-      </div> 
+      </div>  */}
   )
 }
 
